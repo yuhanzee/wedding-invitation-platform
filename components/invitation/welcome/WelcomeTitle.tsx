9aee94a32838d0ef9ad9a45d4688e41835821fb0
Yuhansi <it23245310@my.sliit.lk>
2026-07-11 22:13:08 +0530

    add welcome screen title component

---
+++ components/invitation/welcome/WelcomeTitle.tsx
@@ -0,0 +1,7 @@
+export default function WelcomeTitle() {
+  return (
+    <p className="text-[#6B6B6B] text-[11px] tracking-[0.08em] text-center">
+      WELCOME TO THE WEDDING OF
+    </p>
+  );
+}
