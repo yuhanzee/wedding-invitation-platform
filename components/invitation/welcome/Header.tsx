71def460db0e56d51b6d47499619adc1f7ea0106
Yuhansi <it23245310@my.sliit.lk>
2026-07-12 19:35:59 +0530

    add personalized wedding welcome header

--- components/invitation/welcome/Header.tsx
+++ components/invitation/welcome/Header.tsx
@@ -1,3 +1,29 @@
+// import WelcomeTitle from "./WelcomeTitle";
+// import CoupleNames from "./CoupleNames";
+
+// type HeaderProps = {
+//   groomName: string;
+//   brideName: string;
+// };
+
+// export default function Header({
+//   groomName,
+//   brideName,
+// }: HeaderProps) {
+//   return (
+//     <header className="w-full flex flex-col items-center pt-[100px]">
+//       <WelcomeTitle />
+
+//       <div className="mt-3">
+//         <CoupleNames
+//           groomName={groomName}
+//           brideName={brideName}
+//         />
+//       </div>
+//     </header>
+//   );
+// }
+
 import WelcomeTitle from "./WelcomeTitle";
 import CoupleNames from "./CoupleNames";
 
@@ -11,15 +37,13 @@ export default function Header({
   brideName,
 }: HeaderProps) {
   return (
-    <header className="w-full flex flex-col items-center pt-[100px]">
+    <header className="flex-[3] flex flex-col items-center justify-end pb-6">
       <WelcomeTitle />
 
-      <div className="mt-3">
-        <CoupleNames
-          groomName={groomName}
-          brideName={brideName}
-        />
-      </div>
+      <CoupleNames
+        groomName={groomName}
+        brideName={brideName}
+      />
     </header>
   );
 }
