e561513d653b7fdb95478966e98fe8e7338d767b
Yuhansi <it23245310@my.sliit.lk>
2026-07-17 09:51:34 +0530

    style(invitation): refine invitation typography

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,6 +1,12 @@
-import { Great_Vibes } from "next/font/google";
+import { Great_Vibes, Cormorant_Garamond } from "next/font/google";
 
 export const greatVibes = Great_Vibes({
   weight: "400",
   subsets: ["latin"],
+});
+
+export const cormorantGaramond = Cormorant_Garamond({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  style: ["normal", "italic"],
 });
